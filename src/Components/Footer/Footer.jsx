fba668e5a3744b487075d99fb41af22bf1f145c6
SAEID SHOJAEI <saeidshojaei71@gmail.com>
2023-12-10 20:37:10 +0330

    footer added

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,6 +1,9 @@
 import React from "react";
-import "./Footer.css";
 import { Link } from "react-router-dom";
+import SocialIcon from "../SocialIcon/SocialIcon";
+import { Envelope } from "react-bootstrap-icons";
+
+import "./Footer.css";
 
 export default function Footer() {
   return (
@@ -34,16 +37,139 @@ export default function Footer() {
       <div className="container footer-body">
         <div className="row">
           <div className="col-12 col-md-6 col-lg-4">
-            <div>
-              <Link to="/" className="footer-logo">
+            <div className="me-md-5">
+              <Link
+                to="/"
+                className="footer-logo text-center text-md-start d-block"
+              >
                 J<span className="footer-logo-select">o</span>B
               </Link>
-              <small className="white-50-t d-block mb-4">
+              <small className="white-50-t d-block mb-4 text-center text-md-start">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis
                 culpa quaerat aliquid, nobis in odio eum ducimus porro
                 distinctio voluptatum.
               </small>
+              <div className="d-flex justify-content-center justify-content-md-start">
+                <SocialIcon />
+              </div>
             </div>
+          </div>
+
+          <div className="col-6 col-md-3 col-lg-2 mt-5 mt-md-0">
+            <h5 className="fw-bold white-t">Categories</h5>
+            <nav>
+              <ul>
+                <li>
+                  <Link className="white-t footer-link-item" to="/">
+                    <small>Full Time</small>
+                  </Link>
+                </li>
+                <li>
+                  <Link className="white-t footer-link-item" to="/">
+                    <small>Part Time </small>
+                  </Link>
+                </li>
+                <li>
+                  <Link className="white-t footer-link-item" to="/">
+                    <small>Freelance</small>
+                  </Link>
+                </li>
+                <li>
+                  <Link className="white-t footer-link-item" to="/">
+                    <small>Internship</small>
+                  </Link>
+                </li>
+                <li>
+                  <Link className="white-t footer-link-item" to="/">
+                    <small>Work From Home</small>
+                  </Link>
+                </li>
+              </ul>
+            </nav>
+          </div>
+
+          <div className="col-6 col-md-3 col-lg-2 mt-5 mt-md-0">
+            <h5 className="fw-bold white-t">Company</h5>
+            <nav>
+              <ul>
+                <li>
+                  <Link className="white-t footer-link-item" to="/">
+                    <small>About us</small>
+                  </Link>
+                </li>
+                <li>
+                  <Link className="white-t footer-link-item" to="/">
+                    <small>Leadership</small>
+                  </Link>
+                </li>
+                <li>
+                  <Link className="white-t footer-link-item" to="/">
+                    <small>Careers</small>
+                  </Link>
+                </li>
+                <li>
+                  <Link className="white-t footer-link-item" to="/">
+                    <small>News & Article</small>
+                  </Link>
+                </li>
+                <li>
+                  <Link className="white-t footer-link-item" to="/">
+                    <small>Legal Notice</small>
+                  </Link>
+                </li>
+              </ul>
+            </nav>
+          </div>
+
+          <div className="col-12 col-lg-4 mt-5 mt-lg-0">
+            <div>
+              <h5 className="fw-bold white-t text-center text-md-start">
+                Newsletter
+              </h5>
+              <small className="white-50-t d-block mb-4 text-center text-md-start">
+                Get exclusive deals by signing up to our Newsletter.
+              </small>
+              <input type="email" className="email-user" placeholder="Email" />
+              <button className="btn-c1 w-100 d-flex gap-1 align-items-center justify-content-center">
+                <Envelope className="fs-5" /> SIGN UP
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="container py-4">
+        <hr className="w-100 white-t" />
+      </div>
+
+      <div className="container pb-5">
+        <div className="row">
+          <div className="col-12 col-lg-6">
+            <small className="white-t d-block text-center text-lg-start">
+              Copyright © 2023 Job, All rights reserved. Developer by
+              <span className="green-t"> Saeid-Shojaei.</span>
+            </small>
+          </div>
+          <div className="col-12 col-lg-6">
+            <nav>
+              <ul className="d-flex justify-content-center justify-content-lg-end align-items-center back-link">
+                <li>
+                  <Link className="white-t footer-link-item" to="/">
+                    <small>Term of use</small>
+                  </Link>
+                </li>
+                <li>
+                  <Link className="white-t footer-link-item" to="/">
+                    <small>Privacy policy</small>
+                  </Link>
+                </li>
+                <li>
+                  <Link className="white-t footer-link-item" to="/">
+                    <small>Cookie policy</small>
+                  </Link>
+                </li>
+              </ul>
+            </nav>
           </div>
         </div>
       </div>
---
+++ src/Components/SocialIcon/SocialIcon.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import "./SocialIcon.css";
+import { Linkedin } from "react-bootstrap-icons";
+import { Github } from "react-bootstrap-icons";
+import { Telegram } from "react-bootstrap-icons";
+
+export default function SocialIcon() {
+  return (
+    <div className="d-flex gap-2">
+      <div>
+        <a
+          href="http://www.linkedin.com/in/saeid-shojaei-a8724a233"
+          target="blank"
+          rel="noreferrer"
+          className="SocialIcon"
+        >
+          <Linkedin className="white-t" />
+        </a>
+      </div>
+      <div>
+        <a
+          href="https://github.com/saeidsh777"
+          target="_blank"
+          rel="noreferrer"
+          className="SocialIcon"
+        >
+          <Github className="white-t" />
+        </a>
+      </div>
+      <div>
+        <a
+          href="https://t.me/saeidshojaei777"
+          target="blank"
+          rel="noreferrer"
+          className="SocialIcon"
+        >
+          <Telegram className="white-t" />
+        </a>
+      </div>
+    </div>
+  );
+}
