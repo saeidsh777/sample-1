0c774ed7891efdc0e7097633921a0dca6cecda86
SAEID SHOJAEI <saeidshojaei71@gmail.com>
2023-12-14 15:13:58 +0330

    about company added

---
+++ src/Components/AboutCompany/AboutCompany.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import './AboutCompany.css'
+import { BriefcaseFill, PatchCheckFill, PeopleFill, PersonFill } from 'react-bootstrap-icons';
+import { Link } from 'react-router-dom';
+export default function AboutCompany(props) {
+  return (
+    <div className="about-company white-b">
+      <span className="valid-company d-flex align-items-center gap-2">
+        <PatchCheckFill className="green-t" />
+        <span className="valid-title">Verified Company</span>
+      </span>
+
+      <div className="d-flex gap-3 align-items-center mt-3">
+        <div className="logo-box-img">
+          <img src={props.logo} alt="company logo" />
+        </div>
+        <div className="title-job-box">
+          <small className="black-50-t">{props.location}</small>
+          <Link to={`/job/${props.jobID}`} className="h5 fw-bold m-0 d-block">
+            {props.jobTitle}
+          </Link>
+        </div>
+      </div>
+
+      <hr className="w-100" />
+
+      <ul>
+        <li className="d-flex align-items-center gap-2 mb-2">
+          <PeopleFill className="green-t" />
+          <p className="black-50-t m-0">Size: 1,001 - 5,000 employees</p>
+        </li>
+        <li className="d-flex align-items-center gap-2 mb-2">
+          <PersonFill className="green-t" />
+          <p className="black-50-t m-0">Any Experience Welcomed</p>
+        </li>
+        <li className="d-flex align-items-center gap-2">
+          <BriefcaseFill className="green-t" />
+          <p className="black-50-t m-0">Industry: Healthcare, Insurance</p>
+        </li>
+      </ul>
+    </div>
+  );
+}
--- src/pages/JobInfos/JobInfos.jsx
+++ src/pages/JobInfos/JobInfos.jsx
@@ -5,9 +5,9 @@ import Header from "../../Components/Header/Header";
 import jobDatas from "../../data/jobData";
 import Footer from "../../Components/Footer/Footer";
 
-
 import "./JobInfos.css";
 import JobDetail from "../../Components/JobDetail/JobDetail";
+import AboutCompany from "../../Components/AboutCompany/AboutCompany";
 export default function JobInfos() {
   const [mainJob, setMainJob] = useState({});
   const { jobID } = useParams();
@@ -25,10 +25,43 @@ export default function JobInfos() {
             <div className="col-12 col-xl-8">
               <JobDetail {...mainJob} />
             </div>
+
+            <div className="col-12 col-xl-4 mt-3 mt-xl-0">
+              <div className="row">
+                <div className="col-12">
+                  <AboutCompany {...mainJob} />
+                </div>
+
+                <div className="col-12 mt-3">
+                  <div style={{overflow:'hidden', borderRadius:'10px !important'}}>
+                    <iframe
+                      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2033.4330400786782!2d51.427912287746516!3d35.80705727675101!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3f8e0607d6e99c43%3A0x9561c9380a60717f!2sTajrish%20Square%2C%20Tajrish%2C%20Iran!5e0!3m2!1sen!2sau!4v1702376328114!5m2!1sen!2sau"
+                      width="100%"
+                      style={{
+                        border: "0",
+                        minHeight: "300px",
+                        height: "100%",
+                      }}
+                      allowFullScreen=""
+                      loading="lazy"
+                      referrerPolicy="no-referrer-when-downgrade"
+                    ></iframe>
+                  </div>
+                </div>
+
+                <div className="col-12 mt-3">
+                  <div className="green-b">
+                    
+                  </div>
+
+                </div>
+
+              </div>
+            </div>
           </div>
         </div>
       </section>
-      <Footer/>
+      <Footer />
     </div>
   );
 }
